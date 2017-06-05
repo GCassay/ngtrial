b913ffe4fa84e994edc00f1d90eda544258f6dfe
Gea <geacassi@cobogpgp7790.global.publicisgroupe.net>
2017-06-05 17:03:38 -0500

    status icon

--- src/components/trial-block/trial-block.component.js
+++ src/components/trial-block/trial-block.component.js
@@ -2,7 +2,9 @@
 
 angular .module('trialBlock')
         .component('trialBlock', {
+
           templateUrl: "./components/trial-block/trial-block.template.html",
+
           bindings: {
             toggleBlock: '&',
             updateTrials: '=',
@@ -10,5 +12,38 @@ angular .module('trialBlock')
             trials: '=',
             trial: '=',
             activeTrial: '<'
-          }
+          },
+
+          controller: trialBlockController
         });
+
+function trialBlockController() {
+  var vm = this;
+  vm.statusicon = "";
+
+  switch(vm.trial.status){
+    case '0':
+      vm.statusicon = "trial-pending";
+      break;
+    case '1':
+      vm.statusicon = "trial-accept";
+      break;
+    case '2':
+      vm.statusicon = "trial-reject";
+      break;
+  }
+
+  vm.changeIcon = function(icon) {
+    switch(icon){
+      case '0':
+        vm.statusicon = "trial-pending";
+        break;
+      case '1':
+        vm.statusicon = "trial-accept";
+        break;
+      case '2':
+        vm.statusicon = "trial-reject";
+        break;
+    }
+  }
+}
